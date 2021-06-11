9add96e9795e9e62001037f271bcdacff9001444
jigse <hackerworld9869@gmail.com>
2021-06-11 11:44:32 +0530

    1)Create App Drawer

--- lib/widget/drawerScreen.dart
+++ lib/widget/drawerScreen.dart
@@ -1,13 +1,77 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
 
-class DrawerScreen extends StatelessWidget{
+class DrawerScreen extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      color:HexColor("#121212"),
+  _DrawerScreenState createState() => _DrawerScreenState();
+}
 
+class _DrawerScreenState extends State<DrawerScreen>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 3),
+      vsync: this,
     );
+    _animation = Tween(begin: 0.0, end: 1.0).animate(_controller);
   }
 
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+    _controller.forward();
+    return Container(
+      color: HexColor("#121212"),
+      child: Column(
+        children: [
+          Container(
+
+            margin: const EdgeInsets.only(left: 25,top: 40),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundColor: Colors.blueAccent,
+                      child: AutoSizeText(
+                        'J',
+                        style: GoogleFonts.poppins(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    ),
+                    SizedBox(
+                      width: width * 0.07,
+                    ),
+                    FadeTransition(
+                      opacity: _animation,
+                      child: AutoSizeText(
+                        "Jayesh Jain",
+                        style: GoogleFonts.poppins(
+                            fontSize: 22, color: Colors.white),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
