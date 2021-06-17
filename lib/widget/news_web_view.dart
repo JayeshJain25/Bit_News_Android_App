5d517545fef1a10ffac6e15c31e57c6f0e43cfe9
jigse <hackerworld9869@gmail.com>
2021-06-17 18:58:50 +0530

    1)Worked on Web View Implementation

--- lib/widget/news_web_view.dart
+++ lib/widget/news_web_view.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:like_button/like_button.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class NewsWebView extends StatelessWidget {
+class NewsWebView extends StatefulWidget {
+  @override
+  _NewsWebViewState createState() => _NewsWebViewState();
+}
+
+class _NewsWebViewState extends State<NewsWebView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,6 +20,24 @@ class NewsWebView extends StatelessWidget {
           "News",
           style: GoogleFonts.rubik(color: Colors.white),
         ),
+        actions: <Widget>[
+          LikeButton(
+            size: 25,
+            circleColor:
+                CircleColor(start: Color(0xff00ddff), end: Color(0xff0099cc)),
+            bubblesColor: BubblesColor(
+              dotPrimaryColor: Color(0xff33b5e5),
+              dotSecondaryColor: Color(0xff0099cc),
+            ),
+            likeBuilder: (bool isLiked) {
+              return Icon(
+                Icons.bookmark,
+                color: isLiked ? HexColor("#4E8799") : Colors.grey,
+                size: 25,
+              );
+            },
+          ),
+        ],
         centerTitle: true,
       ),
       body: WebView(
---
+++ lib/widget/top_news.dart
@@ -0,0 +1,74 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class TopNews extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+    return Container(
+      width: width / 1.2,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+              colors: [HexColor('#491f01'), Colors.black])),
+      child: Stack(children: <Widget>[
+        Positioned(
+          right: 15,
+          child: ColorFiltered(
+            colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(0.15), BlendMode.dstIn),
+            child: Image(
+                height: height / 3,
+                width: width / 2,
+                fit: BoxFit.contain,
+                image: NetworkImage(
+                    'https://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/1024/Bitcoin-BTC-icon.png')),
+          ),
+        ),
+        Positioned(
+          left: 25,
+          child: Image(
+              height: height / 9,
+              width: width / 9,
+              fit: BoxFit.contain,
+              image: NetworkImage(
+                  'https://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/1024/Bitcoin-BTC-icon.png')),
+        ),
+        Positioned(
+          top: 70,
+          left: 28,
+          child: Container(
+            width: width * 0.2,
+            height: height * 0.03,
+            child: AutoSizeText(
+              'BTC',
+              style: GoogleFonts.rubik(fontSize: 18, color: Colors.white),
+            ),
+          ),
+        ),
+        Positioned(
+          top: 100,
+          child: Container(
+              padding: const EdgeInsets.all(16.0),
+              width: width * 0.81,
+              height: height * 0.23,
+              child: AutoSizeText(
+                'MATIC is Innovative, but It Looks as If It Might Not Be Very Useful',
+                minFontSize: 14,
+                maxLines: 4,
+                style: GoogleFonts.poppins(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              )),
+        ),
+      ]),
+    );
+  }
+}
